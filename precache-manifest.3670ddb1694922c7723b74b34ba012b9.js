71bb421535678dd8de7c22f518a02b4ca1c8b04a
FERREIRA, JONATHAN IVAN <joferreira@santandertecnologia.com.ar>
2020-05-26 21:08:08 -0300

    static sources

---
+++ precache-manifest.3670ddb1694922c7723b74b34ba012b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4afc54b9446331c3119c013e0ac85c9",
+    "url": "/tetris-react-js/index.html"
+  },
+  {
+    "revision": "1289b8245385035fb73a",
+    "url": "/tetris-react-js/static/css/main.a88e5cf1.chunk.css"
+  },
+  {
+    "revision": "c3349c32f5838f288909",
+    "url": "/tetris-react-js/static/js/2.bb47f85f.chunk.js"
+  },
+  {
+    "revision": "1289b8245385035fb73a",
+    "url": "/tetris-react-js/static/js/main.335c7949.chunk.js"
+  },
+  {
+    "revision": "e837f929154127cec7fa",
+    "url": "/tetris-react-js/static/js/runtime~main.692751fe.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/tetris-react-js/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/tetris-react-js/static/media/bg.2810fe6b.png"
+  }
+]);
